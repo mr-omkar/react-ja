315205eab98056db936bd79da76e503651fac004
mr-omkar <desaiomkararun9595@gmail.com>
2020-08-27 21:11:18 +0530

    1 st day

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const name="Omkar"
 const age=17
 
 ReactDOM.render(
-  <> <h1> Welcome to my page </h1>,
+  <> <h1> Welcome to my page </h1>
       <p> Name : {name} </p>
       <p> Age : {age + 1} </p>
+      <p> Age : {console.log("hi")} </p>
   </>
   , document.getElementById("root")
   );
